c97a5c5f25fc6182fc3fd0b1fe7dcc629d685a4e
Nirvik Purkait <nirvikpurkait@gmail.com>
2025-07-28 22:46:53 +0530

    modal improved

--- src/popup.tsx
+++ src/popup.tsx
@@ -10,7 +10,7 @@ export default function Popup({ ...props }: React.ComponentProps<"div">) {
           {...props}
           className={`backdrop-blur-xs flex gap-4 z-[9999] text-gray-50 text-2xl flex-col absolute top-0 inset-0  justify-center items-center ${props.className}`}
         >
-          <div className="border px-4 py-2 rounded-lg animate-appear">
+          <div className="border px-4 py-2 rounded-lg animate-appear relative">
             <p>Exam is completed</p>
             <Link to={"/"}>Go to home screen</Link>
           </div>
